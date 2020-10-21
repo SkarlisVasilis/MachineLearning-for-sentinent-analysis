2fa2701c7db0babac4d1b0c685588ac9ce22aab3
seilver <31968843+seilver@users.noreply.github.com>
2020-10-21 20:10:25 +0300

    Add files via upload

--- Scrapper.py
+++ Scrapper.py
@@ -5,16 +5,20 @@ from time import sleep
 import io
 import os
 
-
+# To later store the url for each abstract page
 all_urls = []
+# To later store the filename for each abstract page
 filenames = []
 
+# Keep the full urls for each abstract page
 pages = []
+# Check the pages 15 to 25, of the search results from the query: Death rate due to road traffic+injuries
 page_numbers = list(range(15, 26))
 for page_number in page_numbers :
     page = "https://pubmed.ncbi.nlm.nih.gov/?term=" + "Death+rate+due+to+road+traffic+injuries" + "&page=" + str(page_number)
     pages.append(page)
 
+# For each page url, keep the id of the page.
 for page in pages :
     html = requests.get(page).content
     soup = BeautifulSoup(html, 'html.parser')
@@ -32,7 +36,7 @@ for page in pages :
     sleep(1)
 
 
-
+# Extract Titles and Abstracts from each page
 titles = []
 abstracts = []
 
@@ -41,6 +45,7 @@ for url in all_urls :
     html = requests.get(url).content
     soup = BeautifulSoup(html, 'html.parser')
 
+# Try to find title of page. If not keep value "NO TITLE"
     try :
         title = soup.find("h1", class_="heading-title").text
         title = title.replace("\n", "").lstrip().rstrip()
@@ -49,6 +54,7 @@ for url in all_urls :
         title = "NO TITLE"
         titles.append(title)
 
+    # Try to find content text of page. If not keep value "NO ABSTRACT"
     try :
         abstract = soup.find("div", class_="abstract-content selected").text
         abstract = abstract.replace("\n", "").lstrip().rstrip()
@@ -58,16 +64,18 @@ for url in all_urls :
         abstracts.append(abstract)
     sleep(1)
 
+# Create a Metadata dataframe that contains the filename, url and title of each page we extracted information from.
+# We then export it as an excel file.
 Articles_Batch_Metadata = pd.DataFrame({'Filename': filenames,'Url': all_urls,'Title': titles})
 Articles_Batch_Metadata.to_excel (r'batch_temp_index.xlsx', index = False, header=True)
 
 print(Articles_Batch_Metadata[['Url', 'Filename']])
 
-# Write txt files.
-# Create folder that contains the txt files
+# Create batch_temp folder that contains the txt files. Each txt file will contain the title of each page and its equivalent abstract.
 if not os.path.exists('batch_temp'):
     os.makedirs('batch_temp')
 
+# Write txt files on folder batch_temp.
 for url in all_urls:
     path = r"batch_temp/" + filenames[all_urls.index(url)]
     with io.open(path, "w", encoding="utf-8") as f:
